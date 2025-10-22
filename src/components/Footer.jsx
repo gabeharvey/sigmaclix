4bb1f711307b8c2bcba26d00d603772a74ad4975
Gabe Harvey <harvey7@gmail.com>
2025-10-22 17:13:10 -0500

    Updated Address on Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,6 +18,7 @@ function Footer() {
       boxShadow="0 -4px 10px rgba(0,0,0,0.3)"
     >
       <Flex
+        direction="column"
         justify="center"
         alignItems="center"
         position="relative"
@@ -26,28 +27,44 @@ function Footer() {
         flexWrap="wrap"
       >
         <Text
-            fontFamily="'Bangers', system-ui"
-            fontSize={['2xl', '3xl']}
-            fontWeight="bold"
-            letterSpacing="wider"
-            whiteSpace={{ base: 'normal', md: 'nowrap' }}
-            color="#FFD500"
-            textShadow="2px 2px 4px rgba(255, 255, 255, 0.7)" 
-            style={{
-                WebkitTextStroke: "1px white", 
-            }}
+          fontSize={['2xl', '3xl']}
+          fontWeight="bold"
+          letterSpacing="wider"
+          whiteSpace={{ base: 'normal', md: 'nowrap' }}
+          color="#FFD500"
+          textShadow="2px 2px 4px rgba(255, 255, 255, 0.7)"
+          style={{ WebkitTextStroke: '1px white' }}
         >
-            ©2025{' '}
-            <Box
-                as="span"
-                fontFamily="'Bangers', system-ui"
-                color="#FFD500"
-                display="inline"
-                textShadow="2px 2px 4px rgba(255, 255, 255, 0.7)"
-                style={{ WebkitTextStroke: "1px white" }}
-            >
-                Sigma Clix LLC
-            </Box>
+          ©2025{' '}
+          <Box
+            as="span"
+            color="#FFD500"
+            display="inline"
+            textShadow="2px 2px 4px rgba(255, 255, 255, 0.7)"
+            style={{ WebkitTextStroke: '1px white' }}
+          >
+            Sigma Clix LLC
+          </Box>
+        </Text>
+
+        {/* Company Address */}
+        <Text
+                            mt="0.25rem"
+                            fontFamily="'Luckiest Guy', system-ui"
+                            fontSize={{ base: '.9rem', md: '1.05rem', lg: '1.2rem' }} 
+                            fontWeight="bold"
+                            textAlign="center"
+                            lineHeight="1.2"
+                            color="#00B3B3"
+                            letterSpacing="0.5px"
+                            textShadow="
+                                -1px -1px 0 #fff,
+                                1px -1px 0 #fff,
+                                -1px  1px 0 #fff,
+                                1px  1px 0 #fff
+                            "
+        >
+          Austin, TX
         </Text>
       </Flex>
     </Box>
